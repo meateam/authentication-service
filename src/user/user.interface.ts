7ad793f7e642481f9a436a65cb3f88f27af4609c
Allkinds <yonatald@gmail.com>
2019-12-16 11:04:26 +0200

    style
    Signed-off-by: Allkinds <yonatald@gmail.com>

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,12 +1,12 @@
 export interface IUser {
-	id: string;
-	name?: Name;
-	firstName: string;
-	lastName: string;
-	fullName?: string;
-	mail: string;
-	hierarchy?: string[];
-	hierarchyFlat?: string;
+    id: string;
+    name?: Name;
+    firstName: string;
+    lastName: string;
+    fullName?: string;
+    mail: string;
+    hierarchy?: string[];
+    hierarchyFlat?: string;
 }
 
 interface Name {
